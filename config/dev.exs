2775e190ad642a612eb013b14dcfd25a94376ec5
Ladislav Szolik <szolik.ladislav@gmail.com>
2022-01-09 18:13:51 +0100

    Add TailwindCSS with Forms

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,16 @@ config :longform, LongformWeb.Endpoint,
   secret_key_base: "EmzBfPSXhexUgQ3uNCir4PV0Qw6d6zwM178BrH5+Qs6oduzb2TIWriERxL89Ztbk",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    # watch tailwind for changes
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
